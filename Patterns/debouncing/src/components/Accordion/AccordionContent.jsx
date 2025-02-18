e865706d3288f356ac7fff3e3652d16d244fef81
Suchismita Swain <123295737+suchismitaswain@users.noreply.github.com>
2025-02-18 23:27:59 +0530

    Update AccordionContent.jsx

--- Patterns/debouncing/src/components/Accordion/AccordionContent.jsx
+++ Patterns/debouncing/src/components/Accordion/AccordionContent.jsx
@@ -1,6 +1,7 @@
 import { useAccordionContext } from './Accordion.jsx';
 import { useAccordionItemContext } from './AccordionItem.jsx';
 
+
 export default function AccordionContent({ className, children }) {
   const { openItemId } = useAccordionContext();
   const id = useAccordionItemContext();
